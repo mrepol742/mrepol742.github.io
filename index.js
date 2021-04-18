3f1d90052afcb786a49f75303337a5919ee220f0
mrepol742 <mrepol742@protonmail.com>
2021-04-18 14:50:47 +0800

    Initial Commit

--- index.js
+++ index.js
@@ -1,32 +1,9 @@
-function project_webvium() {
-    window.location.href = "https://github.com/mrepol742/PROJECT-WEBVIUM";
-}
-
-function mrepol742_github_io() {
-    window.location.href = "https://github.com/mrepol742/mrepol742.github.io";
-}
-
-function server() {
-    window.location.href = "https://github.com/mrepol742/Server";
-}
-
-function mrepol742() {
-    window.location.href = "https://github.com/mrepol742/mrepol742";
-}
-
-function hello_world() {
-    window.location.href = "https://github.com/mrepol742/Hello_World";
-}
-
-
 function openNav() {
   document.getElementById("mrepol742_sidebar").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
 }
 
 function closeNav() {
   document.getElementById("mrepol742_sidebar").style.width = "0";
-  document.getElementById("main").style.marginLeft= "0";
 }
 
 function mrepol742_github_io_2() {
