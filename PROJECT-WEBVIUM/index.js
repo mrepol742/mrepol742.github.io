eb82456ba33fa61d85adbccb49d1694b6d377fc6
mrepol742 <mrepol742@protonmail.com>
2021-05-04 10:47:43 +0800

    Initial commit

--- PROJECT-WEBVIUM/index.js
+++ PROJECT-WEBVIUM/index.js
@@ -70,7 +70,7 @@ var firebaseConfig = {
 function ex(link) {
 	e.preventDefault();
         grecaptcha.ready(function() {
-          grecaptcha.execute('reCAPTCHA_site_key', {action: 'submit'}).then(function(token) {
+          grecaptcha.execute('6Lf7QMUaAAAAAAdE2gqKY9ob3Q5hJ9sIRZCb-FcS', {action: 'submit'}).then(function(token) {
              window.location.href = link;
           });
         });
