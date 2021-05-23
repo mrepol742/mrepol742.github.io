9d9c7587f46c74767667659f453bffc97bfad38d
mrepol742 <mrepol742@protonmail.com>
2021-05-23 16:29:41 +0800

    inital commit

--- PROJECT-WEBVIUM/TermsAndConditions/index.js
+++ PROJECT-WEBVIUM/TermsAndConditions/index.js
@@ -15,9 +15,8 @@
 * limitations under the License.
 */
 
-
 function openNav() {
-  document.getElementById("mrepol742_sidebar").style.width = "250px";
+  document.getElementById("mrepol742_sidebar").style.transform = 'translateX(0%)';
   var bg = document.createElement('div')
 bg.setAttribute('class', 'sidebar-bg')
 bg.setAttribute('id', 'sbg')
@@ -28,7 +27,7 @@ document.getElementById('sbg').addEventListener('click', () => {
 }
 
 function closeNav() {
-  document.getElementById("mrepol742_sidebar").style.width = "0";
+  document.getElementById("mrepol742_sidebar").style.transform = 'translateX(-100%)';
   var bg = document.getElementById('sbg')
  document.body.removeChild(bg)
 }
