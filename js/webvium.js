18a054eab1a288c51f37017cf3f812bba7206c42
mrepol742 <mrepol742@protonmail.com>
2021-05-24 14:28:30 +0800

    Inital commit

--- js/webvium.js
+++ js/webvium.js
@@ -133,3 +133,18 @@ let downloaded = (ip) => {
 		IP: ip
 	})
 }
+
+window.fbAsyncInit = function() {
+	FB.init({
+		xfbml: true,
+		version: 'v10.0'
+	});
+};
+
+(function(d, s, id) {
+	var js, fjs = d.getElementsByTagName(s)[0];
+	if (d.getElementById(id)) return;
+	js = d.createElement(s); js.id = id;
+	js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
+	fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
