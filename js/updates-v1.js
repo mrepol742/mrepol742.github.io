18c0ea86c52c4eace656d156434bfbeeb9b4f5c5
mrepol742 <mrepol742@protonmail.com>
2021-07-31 15:53:38 +0800

    Initial Commit

--- js/updates-v1.js
+++ js/updates-v1.js
@@ -1,8 +1,9 @@
 let webvium = ['Webvium-v21.04.17', 'Webvium-v21.4.20', 'Webvium-v1.2', 'Webvium-v1.3', 'Webvium-v1.4', 'Webvium-v1.5', 'Webvium-v1.6', 'Webvium-v1.7']
+let da = document.getElementsByClassName("mrepol742_sidebar");
 webvium.forEach(function(item, index, array) {
   console.log(item, index)
   let li0 = document.createElement("a")
   li0.href = 'https://mrepol742.github.io/PROJECT-WEBVIUM/Downloads/' + item + '.apk';
   li0.title = item;
-  div.appendChild(li0)
+  da.appendChild(li0)
 })
