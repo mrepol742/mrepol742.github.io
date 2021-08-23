1275f3a4a6ccc3e915507266aa14a721fe8aa3bd
mrepol742 <mrepol742@protonmail.com>
2021-08-23 16:58:08 +0800

    Initial Commit

--- js/webvium.js
+++ js/webvium.js
@@ -5,19 +5,17 @@ let items = document.querySelectorAll('.size12')
 items.forEach((e) => {
     e.innerHTML = "538KB";
 })
-document.querySelector('#download_btn').textContent = 'Webvium v1.8.1 (538KB)';
+document.querySelector('#download_btn').textContent = 'Webvium v1.8.1 --538KB--';
 function download() {
 	window.location.href = "https://github.com/webvium/webvium.github.io/blob/main/Webvium%20v1.8.1-stable.apk?raw=true";
 }
 try {
 
 if (a > Webvium.currentVersion()) {
-document.getElementById("curr").innerHTML = "You are using an outdated version of Webvium.";
-document.getElementById("curr").style.color = "#ea4335";
+document.getElementById("curr").innerHTML = "<div class=\"alert alert-danger\" role=\"alert\">You are using an outdated version of Webvium.</div>";
 } else {
-document.getElementById("curr").innerHTML = "No need to Download. You are already using the Latest version.";
-document.getElementById("curr").style.color = "#4285f4";
+document.getElementById("curr").innerHTML = "<div class=\"alert alert-success\" role=\"alert\">No need to Download. You are already using the Latest version.</div>";
 }
 } catch (en) {
-
+    document.getElementById("curr").innerHTML = "<div class=\"alert alert-info\" role=\"alert\">By downloading Webvium, you agree to its <a href=\"https://mrepol742.github.io/PROJECT-WEBVIUM/PrivacyPolicy\">Privacy Policy</a></div>";
 }
