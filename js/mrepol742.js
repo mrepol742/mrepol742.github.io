24c99116fb016027571d3a33d830872f63ea29dc
mrepol742 <mrepol742@protonmail.com>
2021-11-02 23:27:37 +0800

    Initial Commit

--- js/mrepol742.js
+++ js/mrepol742.js
@@ -1,3 +1,20 @@
+/*
+*
+* Copyright (c) 2021 Melvin Jones Repol (mrepol742.github.io). All rights reserved.
+*
+* License under the GNU General Public License, Version 3.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     https://www.gnu.org/licenses/gpl-3.0.en.html
+*
+* Unless required by the applicable law or agreed in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
 window.addEventListener('scroll', reveal)
 reveal();
 lozad().observe();
