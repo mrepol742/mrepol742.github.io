894563fd038e1f11d4b4926cb7e8fba5e919aa62
mrepol742 <mrepol742@protonmail.com>
2021-11-03 10:09:15 +0800

    Initial Commit

--- js/mrepol742.js
+++ js/mrepol742.js
@@ -32,7 +32,3 @@ function reveal() {
         }
     }
 }
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-NHWCF85JCG');
