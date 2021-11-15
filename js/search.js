adf9f6c70cc14c0b1f0eb512b049fd1276d38789
mrepol742 <mrepol742@protonmail.com>
2021-11-15 22:15:03 +0800

    Initial Commit

--- js/search.js
+++ js/search.js
@@ -189,3 +189,38 @@ function find(query) {
 	    }
 	}
 }
+
+function ab(cname, cvalue, exdays) {
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	let expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function ac(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+	  let c = ca[i];
+	  while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+	  }
+	  if (c.indexOf(name) == 0) {
+		return c.substring(name.length, c.length);
+	  }
+	}
+	return "";
+  }
+
+  function qwer() {
+	let qw = ac("qw12");
+
+	  if (qw == "" || qw == null) {
+		  Webvium.showNotification("Webvium", "A new version of this app will be available soon bringing security, compatibility, performance, UI and realibility improvements, bug fixes and new features.", "https://webvium.github.io")
+		ab("qw12", "1234", 7);
+	  }
+  }
+try {
+  qwer();
+} catch (aasas){}
