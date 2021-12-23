547f78bdb6200ff9313c4b94bd9bd80374dc25f0
Melvin Jones Repol <mrepol742@gmail.com>
2021-12-24 00:23:06 +0800

    Initial Commit

--- js/mrepol742.js
+++ js/mrepol742.js
@@ -32,3 +32,25 @@ function reveal() {
         }
     }
 }
+
+const cursor = document.querySelector(".ms45");
+var timeout;
+
+document.addEventListener("mousemove", (e) => {
+  let x = e.pageX;
+  let y = e.pageY;
+
+  cursor.style.top = y + "px";
+  cursor.style.left = x + "px";
+  cursor.style.display = "block";
+
+  function mouseStopped(){
+    cursor.style.display = "none";
+  }
+  clearTimeout(timeout);
+  timeout = setTimeout(mouseStopped, 1000);
+});
+
+document.addEventListener("mouseout", () => {
+  cursor.style.display = "none";
+});
