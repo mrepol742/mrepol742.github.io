bf9e8c521c1f50763fa744044a190bb2504e812d
Melvin Jones Repol <mrepol742@gmail.com>
2022-01-05 14:30:52 +0800

    Initial Commit

--- js/mj.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    setTimeout(function() {
-      if ( typeof(window.google_jobrunner) === "undefined" ) {
-        console.log("ad blocker installed");
-      } else {
-        console.log("no ad blocking found.");
-      }
-    }, 10000);
-  };
