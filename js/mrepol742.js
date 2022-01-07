2a5137b5c9023e5f15f7ebf9ea722370a9038ce3
Melvin Jones Repol <mrepol742@gmail.com>
2022-01-07 15:54:50 +0800

    Initial COommit

--- js/mrepol742.js
+++ js/mrepol742.js
@@ -58,3 +58,45 @@ document.addEventListener("mousemove", (e) => {
 document.addEventListener("mouseout", () => {
   cursor.style.display = "none";
 });
+
+function ab(cname, cvalue, exdays) {
+	const d = new Date();
+	d.setTime(d.getTime() + (exdays*24*60*60*1000));
+	let expires = "expires="+ d.toUTCString();
+	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  }
+
+  function ac(cname) {
+	let name = cname + "=";
+	let decodedCookie = decodeURIComponent(document.cookie);
+	let ca = decodedCookie.split(';');
+	for(let i = 0; i <ca.length; i++) {
+	  let c = ca[i];
+	  while (c.charAt(0) == ' ') {
+		c = c.substring(1);
+	  }
+	  if (c.indexOf(name) == 0) {
+		return c.substring(name.length, c.length);
+	  }
+	}
+	return "";
+  }
+
+
+  function qwer() {
+    let qw = ac("gy5");
+  
+      if (qw == "" || qw == null) {
+        var x = document.querySelectorAll("#tst");
+    x[0].style.setProperty("display", "block", "important");
+      }
+    }
+  try {
+    qwer();
+  } catch (aasas){} 
+
+  function acc() {
+    var x = document.querySelectorAll("#tst");
+    x[0].style.setProperty("display", "none", "important");
+    ab("gy5", "56a", 99);
+  }
