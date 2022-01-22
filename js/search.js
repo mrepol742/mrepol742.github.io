811fd5f035015b9d475e37f064c52a39830ec03b
Melvin Jones Repol <mrepol742@gmail.com>
2022-01-23 00:02:21 +0800

    Initial Commit

--- js/search.js
+++ js/search.js
@@ -16,50 +16,24 @@
 */
 
 lozad().observe();
-var a = 27;
-
-try {
-    if (a > Webvium.currentVersion()) {
-		let qw = ac("qwewqe34");
-		if (qw == "" || qw == null) {
-		  Webvium.showNotification("Update available", "A new version of Webvium is now available to download", "https://mrepol742.github.io/webvium")
-		  ab("oyujt", "f3f4fas", 1);
-		}
-     
-    }
-
-} catch (ee) {
-
-}
-
-
-
-let vh = window.innerHeight * 0.01;
-document.documentElement.style.setProperty('--vh', `${vh}px`)
-document.documentElement.style.setProperty('--sug', `${vh}px`)
 
 try {
 	if (WebviumThemeHelper.isBackgroundEnabled()) {
     if (!WebviumThemeHelper.isCustomBackgroundEnabled()) {
         if (WebviumThemeHelper.isDarkModeEnabled()) {
-            bg.src = 'https://source.unsplash.com/' + WebviumThemeHelper.getQuality() + '?night';
-			document.getElementById("webvium").style.color ="#ffffff";
+            document.body.style.background = "url('https://source.unsplash.com/" + WebviumThemeHelper.getQuality() + "?night')";
+            document.getElementById("webvium").style.color = "#ffffff";
         } else {
-			document.getElementById("webvium").style.color ="#212121";
-            bg.src = 'https://source.unsplash.com/' + WebviumThemeHelper.getQuality() + '?day';
+            document.body.style.background = "url('https://source.unsplash.com/" + WebviumThemeHelper.getQuality() + "?day')";
+            document.getElementById("webvium").style.color = "#484848";
         }
     } else {
-		// bug not solve
-        bg.style.display = 'none';
-		bdy.style.background = 'red';
+        document.body.style.background = "transparent";
     }
 }
 } catch (a) {
-    bg.src = 'https://source.unsplash.com/640x480?day';
+    document.body.style.background = "url('https://source.unsplash.com/640x480?day')";
 }
-
-
-document.getElementById("search").style.background = "linear-gradient(" + Math.floor(Math.random() * 91) + 20 +"deg, rgb(95, 44, 130), rgb(73, 160, 157))";
 
 const node = document.getElementById("search");
 node.addEventListener("keyup", function(event) {
@@ -67,49 +41,6 @@ node.addEventListener("keyup", function(event) {
 	    find(search.value)
     }
 });
-
-try {
-    var su = WebviumSearchHelper.query().split(":");
-    if (su != "null") {
-        for (let i = 0; i < su.length; i++) {
-            let sugItem = document.createElement('div')
-            sugItem.setAttribute('class', 'sug-item')
-
-            let icon = document.createElement('span')
-            icon.setAttribute('class', 'material-icons')
-            icon.textContent = 'history'
-
-            let text = document.createElement('span')
-            text.setAttribute('class', 'text')
-            text.textContent = atob(su[i])
-
-            sugItem.appendChild(icon)
-            sugItem.appendChild(text)
-            sug.appendChild(sugItem)
-
-            search.addEventListener('input', () => {
-                if (search.value.length != 0) {
-                    sug.style.display = 'flex'
-                    if (text.textContent.includes(search.value)) {
-                        sugItem.style.display = 'flex'
-                        sug.style.display = 'flex'
-                    } else {
-                        sugItem.style.display = 'none'
-                    }
-
-                    sugItem.addEventListener('click', () => {
-                        search.value = atob(su[i])
-                    })
-                } else {
-                    sug.style.display = 'none'
-                    search.style.transform = 'translateY(0%)'
-                    webvium.style.transform = 'translateY(0%)'
-                    sug.style.transform = 'tranlate(-50%, 0%)'
-                }
-            })
-        }
-    }
-} catch (a) {}
 
 /*
  * Roses are red
@@ -201,38 +132,3 @@ function find(query) {
 	    }
 	}
 }
-
-function ab(cname, cvalue, exdays) {
-	const d = new Date();
-	d.setTime(d.getTime() + (exdays*24*60*60*1000));
-	let expires = "expires="+ d.toUTCString();
-	document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-  }
-
-  function ac(cname) {
-	let name = cname + "=";
-	let decodedCookie = decodeURIComponent(document.cookie);
-	let ca = decodedCookie.split(';');
-	for(let i = 0; i <ca.length; i++) {
-	  let c = ca[i];
-	  while (c.charAt(0) == ' ') {
-		c = c.substring(1);
-	  }
-	  if (c.indexOf(name) == 0) {
-		return c.substring(name.length, c.length);
-	  }
-	}
-	return "";
-  }
-/*
-  function qwer() {
-	let qw = ac("qw1212");
-
-	  if (qw == "" || qw == null) {
-		  Webvium.showNotification("Webvium", "A new version of this app will be available soon bringing security, compatibility, performance, UI and reliability improvements, bug fixes and new features.", "https://webvium.github.io")
-		ab("qw1212", "1234", 7);
-	  }
-  }
-try {
-  qwer();
-} catch (aasas){} */
