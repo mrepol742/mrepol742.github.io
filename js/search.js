eec45925f08dffe90fce156f8f38877c884108ac
Melvin Jones Repol <mrepol742@gmail.com>
2022-01-27 08:24:51 +0800

    Initial Commit

--- js/search.js
+++ js/search.js
@@ -137,3 +137,22 @@ function find(query) {
 		}
 	}
 }
+
+
+window.addEventListener('scroll', reveal)
+reveal();
+
+function reveal() {
+  let items = document.querySelectorAll('.obj')
+  for (let i = 0; i < items.length; i++) {
+    let windowHeight = window.innerHeight;
+    let revealTop = items[i].getBoundingClientRect().top;
+    let distance = 50;
+
+    if (revealTop < windowHeight - distance) {
+      items[i].classList.add('active')
+    } else {
+      items[i].classList.remove('active')
+    }
+  }
+}
