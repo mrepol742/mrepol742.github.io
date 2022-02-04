aa989f35beeca20fdd393a60faeef1acf755c318
Melvin Jones Repol <mrepol742@gmail.com>
2022-02-05 00:40:57 +0800

    Initial Commit

--- js/webvium.js
+++ js/webvium.js
@@ -47,6 +47,10 @@ function ct() {
         document.getElementById("download_btn").innerHTML = "Download Webvium v" + fo;
         window.location.href = ul + fo + ab;
         resetTimer()
+        ga('send', {
+            hitType: 'event',
+            item_category: "onDownload"
+          });
     } else {
         i2--;
         document.getElementById("download_btn").innerHTML = "Download Starts in " + i2 + " secs";
