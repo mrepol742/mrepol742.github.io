d31a9f1d67e909e98e97e50f297e929881093ddc
Melvin Jones Repol <mrepol742@gmail.com>
2022-02-05 17:06:52 +0800

    Initial Commit

--- js/searchdev.js
+++ js/searchdev.js
@@ -219,23 +219,4 @@ function find(query) {
 	}
 }
 
-
-window.addEventListener('scroll', reveal)
-reveal();
-
-function reveal() {
-  let items = document.querySelectorAll('.obj')
-  for (let i = 0; i < items.length; i++) {
-    let windowHeight = window.innerHeight;
-    let revealTop = items[i].getBoundingClientRect().top;
-    let distance = 50;
-
-    if (revealTop < windowHeight - distance) {
-      items[i].classList.add('active')
-    } else {
-      items[i].classList.remove('active')
-    }
-  }
-}
-
 // if java side fails all the code here is a trash.
