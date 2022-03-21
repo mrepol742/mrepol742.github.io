43a07b8da4c44048590b9f8ced2c7c750493ddfe
mrepol742 <mrepol742@gmail.com>
2022-03-21 21:29:50 +0800

    Initial Commit

--- RSS.java
+++ RSS.java
@@ -6,13 +6,15 @@ class RSS {
 
     public static void main(String[] sg) {
         Runnable re = () -> {
+            String url = "https://rss.app/feeds/cxpzEpVXXhcnweds.xml";
+            String rss = "https://mrepol742.github.io/rss/rss.xml";
             try {
             File folder = new File("rss");
             if (!folder.exists()) {
                 folder.mkdir();
             }
             File fe = new File("rss/rss.xml");
-            URL u = new URL("https://rss.app/feeds/cxpzEpVXXhcnweds.xml");
+            URL u = new URL(url);
             URLConnection is = u.openConnection();
             InputStream inputStream = is.getInputStream();
             InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
@@ -27,7 +29,7 @@ class RSS {
             inputStream.close();
             FileWriter fw = new FileWriter(fe);
             BufferedWriter bufferedWriter = new BufferedWriter(fw);
-            bufferedWriter.write(cd.toString());
+            bufferedWriter.write(cd.toString().replace(url, rss).replace("<generator>https://rss.app</generator>", "<generator>RSS for Melvin Jones Repol</generator>"));
             bufferedWriter.close();
             fw.close();
             System.out.println("Successfully fetch RSS");
