711179aab5d60ca3bba39c52e556558fb376fcc3
mrepol742 <mrepol742@gmail.com>
2022-03-21 11:17:27 +0800

    Initial Commit

---
+++ RSS.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+import java.net.*;
+
+class RSS {
+
+    public static void main(String[] sg) {
+        Runnable re = () -> {
+            try {
+            File fe = new File("rss.xml");
+            URL u = new URL("https://rss.app/feeds/cxpzEpVXXhcnweds.xml");
+            URLConnection is = u.openConnection();
+            InputStream inputStream = is.getInputStream();
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+            BufferedReader c = new BufferedReader(inputStreamReader);
+            String tf5;
+            StringBuilder cd = new StringBuilder();
+            while ((tf5 = c.readLine()) != null) {
+                 cd.append(tf5).append("\n");
+            }
+            c.close();
+            inputStreamReader.close();
+            inputStream.close();
+            FileWriter fw = new FileWriter(fe);
+            BufferedWriter bufferedWriter = new BufferedWriter(fw);
+            bufferedWriter.write(cd.toString());
+            bufferedWriter.close();
+            fw.close();
+            System.out.println("Successfully fetch RSS");
+            } catch (Exception en) {
+                System.out.println("Failed to fetch RSS");
+            }
+        };
+        new Thread(re).start();
+        
+    }
+}
