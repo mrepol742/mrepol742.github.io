a444a10a229d601f0b037a95116cab9d0080285a
mrepol742 <mrepol742@gmail.com>
2022-03-23 16:46:03 +0800

    Initial Commit

--- IndexNow.java
+++ IndexNow.java
@@ -3,22 +3,23 @@ import java.util.*;
 import java.io.*;
 
 class IndexNow {
-    static String[] sg = new String[]{"/news", "/", "/sitemap", "/webvium", "/webviumdev", "/webviumvpn", "/download"};
+    static String[] sg = new String[]{"/news", "/", "/sitemap", "/webvium", "/webviumdev", "/webviumvpn", "/download", "/rss/rss.xml", "/sitemap.xml"};
     static String format = "https://bing.com/indexnow?url=https://mrepol742.github.io%1$s&key=f20756182df645f9b8cf6254654dd5da";
-    public static void main(String[] sg) {
-       
-            for (String da : sg) {
-                Runnable re = ()->{
- try {
-        URL u = new URL(String.format(format, da));
-        URLConnection is = u.openConnection();
-      
-          } catch (Exception e) {
-            System.out.println(e.getMessage());
+    public static void main(String[] sga) throws IOException {
+        for (String da: sg) {
+            url(String.format(format, da));
+            System.out.println(String.format(format, da));
         }
-                };
-                new Thread(re).start();
-            }
       
     }
+
+    private static void url(String url) throws IOException {
+		URL obj = new URL(url);
+		HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+		int responseCode = con.getResponseCode();
+		System.out.println("POST Response Code :: " + responseCode);
+		if (responseCode != HttpURLConnection.HTTP_OK) {
+			System.out.println("Request not worked");
+		}
+	}
 }
