697c0f1210ccfe1fedde26b5a9f21dc5f6b5cac2
mrepol742 <mrepol742@gmail.com>
2022-03-27 21:44:06 +0800

    Initial Commit

--- index.php
+++ index.php
@@ -1,1 +1,62 @@
-<?php include_once("/search/index.html"); ?>
+<!--
+~
+~ Copyright (c) 2022 Melvin Jones Repol (mrepol742.github.io). All rights reserved.
+~
+~ License under the GNU General Public License, Version 3.0 (the "License");
+~ you may not use this file except in compliance with the License.
+~ You may obtain a copy of the License at
+~
+~     https://www.gnu.org/licenses/gpl-3.0.en.html
+~
+~ Unless required by the applicable law or agreed in writing, software
+~ distributed under the License is distributed on an "AS IS" BASIS,
+~ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+~ See the License for the specific language governing permissions and
+~ limitations under the License.
+-->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="icon" href="/favicon.ico" type="image/x-icon">
+	<meta property="og:type" content="website" />
+    <meta charset="utf-8">
+    <meta property="og:image:alt" content="Webvium">
+    <meta property="twitter:image:src" content="/images/webvium22.jpg"/>
+    <meta property="twitter:title" content="Search - Webvium"/>
+    <meta property="twitter:description" content="A search page specifically designed for Webvium."/> 
+    <meta property="og:title" content="Search - Webvium">
+    <meta property="og:url" content="https://mrepol742.github.io/search">
+    
+    <meta property="og:site_name" content="Search - Webvium">
+    
+    <meta property="og:description"
+    content="A search page specifically designed for Webvium.">
+<meta property="og:image" content="/images/webvium22.jpg">
+    <meta name="theme-color" content="#ffffff">
+    <meta charset="UTF-8">
+  
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="description"
+        content="A search page specifically designed for Webvium.">
+    <meta name="author" content="Melvin Jones Repol">
+    <link rel="author" href="/humans.txt" />
+    
+    <meta name="twitter:site" content="@mrepol742" />
+<meta name="twitter:creator" content="@mrepol742" />
+   
+    <meta name="twitter:card" content="summary">
+    
+ 
+    <link rel="shortcut icon" href="/search/favicon.png">
+    <link rel="apple-touch-icon" href="/search/favicon.png">
+    <title>Search - Webvium</title>
+</head>
+<body>
+<?php 
+header("Location: /search");
+die();
+?>
+</body>
+</html>
