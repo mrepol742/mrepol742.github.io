ead3aa49833409b24046afa2e4680d65fefe9de7
mrepol742 <mrepol742@gmail.com>
2022-05-11 19:53:32 +0800

    Initial Commit

--- webvium/update.js
+++ webvium/update.js
@@ -5,3 +5,16 @@ let updateSize = "490KB";
 let downloadUrlPrefix = "https://mrepol742.github.io/webvium/download/?version=v%WEBVIUM%";
 let versionList = ["1.0", "1.1", "1.2", "1.3", "1.4", "1.5", "1.6", "1.7", "1.8", "1.8.1", "1.9", "2.0", "2.1", "2.2", "2.3", "2.4", "2.5", "2.6", "2.7"];
 let sizeList = ["600", "585", "590", "589", "577", "585", "585", "590", "552", "552", "485", "485", "488", "490", "486", "574", "486", "488", "494"];
+
+let latestVersionCodeDev = 28;
+let latestVersionNameDev = "2.8 dev";
+let updateDateDev = "Daily";
+let updateSizeDev = "700KB";
+
+let latestVersionCodeVPN = 11;
+let latestVersionNameVPN = "1.1";
+let updateDateVPN = "Feb 22, 2022";
+let updateSizeVPN = "7MB";
+let downloadUrlPrefixVPN = "https://mrepol742.github.io/webviumvpn/download/?version=v%WEBVIUM%";
+let versionListVPN = ["1.0"];
+let sizeListVPN = ["7"];
