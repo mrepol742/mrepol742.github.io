cb2d50fbd1c39389fed3bded17a47893c349e407
mrepol742 <mrepol742@gmail.com>
2022-06-18 18:24:19 +0800

    Initial Commit

--- js/newyear.js
+++ js/newyear.js
@@ -1,20 +1,3 @@
-/*
-*
-* Copyright (c) 2022 Melvin Jones Repol (mrepol742.github.io). All rights reserved.
-*
-* License under the GNU General Public License, Version 3.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     https://www.gnu.org/licenses/gpl-3.0.en.html
-*
-* Unless required by the applicable law or agreed in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
 var yr = new Date().getFullYear() + 1;
 var future = new Date("Jan 1, " + yr + " 00:00:00").getTime();
 setInterval(function () {
