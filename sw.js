206ded66eb4154cd2937f94c68f57cdfa5733737
mrepol742 <mrepol742@gmail.com>
2022-06-22 17:48:02 +0800

    Initial Commit

--- sw.js
+++ sw.js
@@ -1,22 +1,5 @@
-/*
-*
-* Copyright (c) 2022 Melvin Jones Repol (mrepol742.github.io). All rights reserved.
-*
-* License under the GNU General Public License, Version 3.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     https://www.gnu.org/licenses/gpl-3.0.en.html
-*
-* Unless required by the applicable law or agreed in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*/
-
-const PRECACHE2 = 'precache-v5';
-const RUNTIME = 'runtime-v5';
+const PRECACHE2 = 'precache-v6';
+const RUNTIME = 'runtime-v6';
 
 self.addEventListener('install', event => {
     event.waitUntil(caches.open(PRECACHE2).then(cache => cache.addAll(["/"])).then(self.skipWaiting())
@@ -37,9 +20,7 @@ self.addEventListener('activate', event => {
 
 self.addEventListener('fetch', event => {
     let url = event.request.url;
-    if (url.startsWith(self.location.origin) &&
-        !(url.includes(".html") || url.includes(".css") || url.endsWith(".xml") || url.endsWith(".mp4") ||
-        (url.includes(".js") && !url.includes("/lib/") && !url.includes("/dist/")))) {
+    if (url.startsWith(self.location.origin) && !url.endsWith(".mp4")) {
         event.respondWith(caches.match(event.request)
             .then(cachedResponse => {
                 if (cachedResponse) {
