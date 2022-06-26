519234fb71616d37195d7bf0adf494f6cf83980a
mrepol742 <mrepol742@gmail.com>
2022-06-27 01:22:34 +0800

    Initial Commit

--- js/api-hash.js
+++ js/api-hash.js
@@ -6,21 +6,46 @@ if ('serviceWorker' in navigator) {
     });
 }
 
-let isRandom = true;
-
 try {
+    WebviumHashHelper
+   
     let input = document.getElementById("inlineFormInput");
-    input.addEventListener('input', sha);
-    isRandom = document.getElementById("flexCheckChecked").value;
+    let isRandom = document.getElementById("flexCheckChecked").value;
+    input.addEventListener('input', () => {
+        sha1.textContent = WebviumHashHelper.encodeSHA('SHA-1', input.value, isRandom);
+        sha224.textContent = WebviumHashHelper.encodeSHA('SHA-224', input.value, isRandom);
+        sha256.textContent = WebviumHashHelper.encodeSHA('SHA-256', input.value, isRandom);
+        sha384.textContent = WebviumHashHelper.encodeSHA('SHA-384', input.value, isRandom);
+    });
 
+    let input1 = document.getElementById("inlineFormInput1");
+    let isRandom1 = document.getElementById("flexCheckChecked1").value;
+    input1.addEventListener('input', () => {
+        md5.textContent = WebviumHashHelper.encodeMD5(input1.value, isRandom1);
+    });
+
+    let input11 = document.getElementById("inlineFormInput11");
+    let isRandom11 = document.getElementById("flexCheckChecked11").value;
+    input11.addEventListener('input', () => {
+        xor.textContent = WebviumHashHelper.encodeMD5(input11.value, isRandom11);
+    });
+    
+
+    let input111 = document.getElementById("inlineFormInput111");
+    let input111n = document.getElementById("inlineFormInput111n");
+    input111.addEventListener('input', () => {
+        alert(WebviumHashHelper.encodeCaesar(input111.value, input111n.value) )
+        caesar.textContent = WebviumHashHelper.encodeCaesar(input111.value, input111n.value);
+    });
+
+    let input111a = document.getElementById("inlineFormInput111a");
+    let input111an = document.getElementById("inlineFormInput111an");
+    input111a.addEventListener('input', () => {
+        caesara.textContent = WebviumHashHelper.encodeCaesar(input111a.value, input111an.value);
+    });
 
 } catch (a) {
-
-}
-
-function sha(e) {
-    sha1.textContent = WebviumHashHelper.encodeSHA('SHA-1', e, isRandom);
-    sha224.textContent = WebviumHashHelper.encodeSHA('SHA-224', e, isRandom);
-    sha256.textContent = WebviumHashHelper.encodeSHA('SHA-256', e, isRandom);
-    sha384.textContent = WebviumHashHelper.encodeSHA('SHA-384', e, isRandom);
+    window.location.href = "intent://user?screen_name=webvium_api_hash#Intent;scheme=webvium;S.browser_fallback_url=https%3A%2F%2Fmrepol742.github.io%2Fwebvium%3Futm_source%3Dopen-webvium-browser%26utm_medium%3Dmrepol742.github.io%2Fwebvium%2Fapi%2Fhash;end"
+    var myModal = new bootstrap.Modal(document.getElementById('myModal'), "keyboard")
+    myModal.show()
 }
