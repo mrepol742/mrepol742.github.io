79ee829bfc7bab9c7f11eb88d76a2538e552d47c
mrepol742 <mrepol742@gmail.com>
2022-07-10 15:33:29 +0800

    Initial Commit

--- sw.js
+++ sw.js
@@ -1,41 +1,20 @@
-const PRECACHE2 = 'precache-v10';
-const RUNTIME = 'runtime-v10';
+const cacheName = 'mrepol742v1';
 
-self.addEventListener('install', event => {
-    event.waitUntil(caches.open(PRECACHE2).then(cache => cache.addAll(["/"])).then(self.skipWaiting())
-    );
+self.addEventListener('install', (event) => {
+  event.waitUntil(caches.open(cacheName));
 });
 
-self.addEventListener('activate', event => {
-    const currentCaches = [PRECACHE2,RUNTIME];
-    event.waitUntil(caches.keys().then(cacheNames => {
-        return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-            return Promise.all(cachesToDelete.map(cacheToDelete => {
-                return caches.delete(cacheToDelete);
-            }
-        ));
-    }).then(() => self.clients.claim()));
-});
-
-self.addEventListener('fetch', event => {
-    let url = event.request.url;
-    if (url.startsWith(self.location.origin) && !url.endsWith(".mp4") && !url.endsWith(".log") && (url.startsWith("https://mrepol742.github.io") || url.startsWith("https://cdn.jsdelivr.net"))) {
-        event.respondWith(caches.match(event.request)
-            .then(cachedResponse => {
-                if (cachedResponse) {
-                    return cachedResponse;
-                }
-                return caches.open(RUNTIME)
-                    .then(cache => {
-                        return fetch(event.request)
-                            .then(response => {
-                                return cache.put(event.request, response.clone())
-                                    .then(() => {
-                                        return response;
-                                    });
-                            });
-                    });
-            }));
-    }
+self.addEventListener('fetch', async (event) => {
+  if (event.request.destination === 'image' || event.request.url.contains("/assets/")) {
+    event.respondWith(caches.open(cacheName).then((cache) => {
+      return cache.match(event.request).then((cachedResponse) => {
+        return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
+          cache.put(event.request, fetchedResponse.clone());
+          return fetchedResponse;
+        });
+      });
+    }));
+  } else {
+    return;
+  }
 });
