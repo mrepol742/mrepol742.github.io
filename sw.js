ec6ea51abb5c4dabca65623f27952e9ee6f053c6
mrepol742 <mrepol742@gmail.com>
2022-07-10 19:42:15 +0800

    Initial Commit

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const cacheName = 'mrepol742v1';
+const cacheName = 'mrepol742v2';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName));
 });
 
 self.addEventListener('fetch', async (event) => {
-  if (event.request.destination === 'image' || event.request.url.includes("/assets/") && event.request.url.startsWith("https://mrepol742.github.io/")) {
+  if ((event.request.destination === 'image' || event.request.url.includes("/assets/")) && event.request.url.includes("https://mrepol742.github.io/")) {
     event.respondWith(caches.open(cacheName).then((cache) => {
       return cache.match(event.request).then((cachedResponse) => {
         return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
