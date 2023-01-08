7843d83c56620ffac173d287ce9b8a6880a6c019
Melvin Jones Repol <mrepol742@gmail.com>
2023-01-08 11:23:16 +0800

    Initial Commit

--- js/search.js
+++ js/search.js
@@ -175,8 +175,9 @@ node.addEventListener('input', () => {
 					let sugItem = document.createElement('div');
 					sugItem.setAttribute('class', 'sug-item');
 
+					
 					let icon = document.createElement('span');
-					icon.textContent = '<svg viewBox="0 0 24 24" id="but"><path fill="#fff" d="M9.5,3A6.5,6.5 0 0,1 16,9.5C16,11.11 15.41,12.59 14.44,13.73L14.71,14H15.5L20.5,19L19,20.5L14,15.5V14.71L13.73,14.44C12.59,15.41 11.11,16 9.5,16A6.5,6.5 0 0,1 3,9.5A6.5,6.5 0 0,1 9.5,3M9.5,5C7,5 5,7 5,9.5C5,12 7,14 9.5,14C12,14 14,12 14,9.5C14,7 12,5 9.5,5Z"></path></svg>';
+					icon.textContent = '';
 
 					let text = document.createElement('span');
 					text.setAttribute('class', 'text');
