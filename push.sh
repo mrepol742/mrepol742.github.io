8ecda29a880abe30abde8ed683579a101d36b1ab
Melvin Jones Repol <mrepol742@gmail.com>
2023-04-06 14:04:09 +0800

    Initial Commit

--- push.sh
+++ push.sh
@@ -1,6 +1,12 @@
 javac Sitemap.java
 java Sitemap
 
+javac SitemapImg.java
+java SitemapImg
+
+javac SitemapVid.java
+java SitemapVid
+
 git add .
 git commit -m "Initial Commit"
 git push origin master
