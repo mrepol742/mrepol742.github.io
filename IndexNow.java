5454819bf4475a61598d8dda46fd405e33009257
Melvin Jones Repol <mrepol742@gmail.com>
2023-04-10 23:55:56 +0800

    Initial Commit

--- IndexNow.java
+++ IndexNow.java
@@ -3,7 +3,7 @@ import java.util.*;
 import java.io.*;
 
 class IndexNow {
-    private static String[] sg = new String[]{"/news/", "/", "/sitemap/", "/webvium/", "/webviumbeta", "/webviumdev/", "/webviumvpn/", "/download/", "/rss/rss.xml", "/sitemap.xml", "/link-tree/"};
+    private static String[] sg = new String[]{"/","/webvium/", "/webviumbeta", "/webviumdev/", "/webviumvpn/", "/rss/rss.xml", "/sitemap.xml", "/link-tree/", "/project-orion/", "/search/", "/project-orion/chat/"};
     private static String format = "https://bing.com/indexnow?url=https://mrepol742.github.io%1$s&key=f20756182df645f9b8cf6254654dd5da";
 
     public static void main(String[] sga) throws IOException {
