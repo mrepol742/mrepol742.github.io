3815b8d3d7da85526546657f04d6c87812f9da5f
Samiun Nafis <luea099@gmail.com>
2023-05-05 16:43:40 +0600

    Does this work?

--- js/chat.js
+++ js/chat.js
@@ -1,3 +1,5 @@
+(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
+
 let isLoaded = true;
 let chats = document.getElementById('chats')
 let messages = [];
