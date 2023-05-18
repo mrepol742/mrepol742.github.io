f78e122409174e006e861a4e4f54aad00aab978c
Melvin Jones Repol <mrepol742@gmail.com>
2023-05-18 13:56:40 +0800

    Initial Commit

--- js/chat.js
+++ js/chat.js
@@ -1,4 +1,4 @@
-(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
+//(function () { var script = document.createElement('script'); script.src="//cdn.jsdelivr.net/npm/eruda"; document.body.appendChild(script); script.onload = function () { eruda.init() } })();
 
 let isLoaded = true;
 let chats = document.getElementById('chats')
