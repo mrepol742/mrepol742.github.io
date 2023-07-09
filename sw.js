dfd63654c148bb9b39164835f052a4fc7db093a9
Melvin Jones Repol <mrepol742@gmail.com>
2023-07-09 10:53:55 +0800

    Initial Commit

--- sw.js
+++ sw.js
@@ -1,13 +1,11 @@
-const cacheName = 'mrepol742v6';
+const cacheName = 'mrepol742v6.1';
 
 self.addEventListener('install', (event) => {
   event.waitUntil(caches.open(cacheName));
 });
 
 self.addEventListener('fetch', async (event) => {
-  if ((event.request.destination === 'image' || event.request.url.includes("/assets/")) && event.request.url.includes(atob("aHR0cHM6Ly9tcmVwb2w3NDIuZ2l0aHViLmlv"))) {
-    console.log("destination " + event.request.destination);
-    console.log("url " + event.request.url);
+  if ((event.request.destination === 'image' || event.request.url.includes("/assets/"))) {
     event.respondWith(caches.open(cacheName).then((cache) => {
       return cache.match(event.request).then((cachedResponse) => {
         return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
