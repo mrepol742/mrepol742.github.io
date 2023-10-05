212e07c2618eea052ef330dad13c374261992367
Melvin Jones Repol <mrepol742@gmail.com>
2023-10-05 10:43:54 +0800

    Initial Commit

--- sw.js
+++ sw.js
@@ -15,7 +15,7 @@
  * limitations under the License.
  */
 
-const cacheName = 'mrepol742v6.2';
+const cacheName = 'mrepol742v6.3';
 
 self.addEventListener('activate', event => {
   event.waitUntil(
@@ -37,7 +37,7 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', async (event) => {
   if (event.request.method !== "GET") return;
-  if ((event.request.destination === 'image' || (event.request.url.includes("/assets/") && event.request.url.includes(atob("aHR0cHM6Ly9tcmVwb2w3NDIuZ2l0aHViLmlv"))))) {
+  if ((event.request.destination === 'image' || event.request.url.includes("/assets/") && event.request.url.includes(atob("aHR0cHM6Ly9tcmVwb2w3NDIuZ2l0aHViLmlv")))) {
     event.respondWith(caches.open(cacheName).then((cache) => {
       return cache.match(event.request).then((cachedResponse) => {
         return cachedResponse || fetch(event.request.url).then((fetchedResponse) => {
