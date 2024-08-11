e791ff3069315a40cdd2f9e0f7d6e47d36cd3947
Melvin Jones Repol <mrepol742@gmail.com>
2024-08-11 13:12:34 +0800

    Initial Commit

--- includes/nav.php
+++ includes/nav.php
@@ -1,5 +1,5 @@
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TFD9TXR" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <header class="autohide navbar navbar-expand-lg fixed-top scrolled-up scrolled-up-tr">
+<header class="autohide navbar navbar-expand-lg fixed-top scrolled-up scrolled-up-tr">
     <nav class="container-fluid">
         <a class="navbar-brand" href="/">
             <img src="/favicon.png" alt="Mrepol742 Favicon" class="d-inline-block align-text-top" width="24" height="24">
@@ -11,17 +11,14 @@
         <div class="collapse navbar-collapse" id="navbarText">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                 <li class="nav-item">
-                    <a class="nav-link" href="/link-tree/">Linktree</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/project-orion/">Orion</a>
+                    <a class="nav-link" href="/">Home</a>
                 </li>
                 <li class="nav-item dropdown">
                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        Webvium 
+                        Webvium
                     </a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                        <a class="dropdown-item" href="/webvium/">Webvium Stable</a>
+                        <a class="dropdown-item" href="/webvium/">Webvium</a>
                         <a class="dropdown-item" href="/webviumbeta/">Webvium Beta</a>
                         <a class="dropdown-item" href="/webviumdev/">Webvium Dev</a>
                         <a class="dropdown-item" href="/search/">Webvium Search</a>
@@ -29,27 +26,41 @@
                     </div>
                 </li>
                 <li class="nav-item">
+                    <a class="nav-link" href="/project-orion/">Orion</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="/link-tree/">Linktree</a>
+                </li>
+                <li class="nav-item">
                     <a class="nav-link" href="https://mrepol742.github.io/blog/">Blog</a>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" href="https://hallofcodes.github.io/">Hall of Codes</a>
                 </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="https://github.com/premoza/premoza">Premoza</a>
+                </li>
             </ul>
             <div>
                 <ul class="nav navbar-nav" style="flex-direction: row;">
+                    <li class="nav-item">
+                        <a class="nav-link" href="https://github.com/mrepol742" title="Github">
+                            <i class="fa-brands fa-github"></i>
+                        </a>
+                    </li>
                     <li class="nav-item">
                         <a class="nav-link" href="/melvin-jones-repol-cv.pdf">
                             <i class="fa-regular fa-file-lines" title="CV"></i>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="https://mrepol742.github.io/sitemap"  title="Sitemap">
-                        <i class="fa-solid fa-sitemap"></i>
+                        <a class="nav-link" href="https://mrepol742.github.io/sitemap" title="Sitemap">
+                            <i class="fa-solid fa-sitemap"></i>
                         </a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="https://mrepol742.github.io/rss"  title="RSS">
-                        <i class="fa-solid fa-rss"></i>
+                        <a class="nav-link" href="https://mrepol742.github.io/rss" title="RSS">
+                            <i class="fa-solid fa-rss"></i>
                         </a>
                     </li>
                 </ul>
